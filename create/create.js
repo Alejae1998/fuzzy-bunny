efc02b4a8c0998de54146afdde964087f620769a
Alejae1998 <alejagr1998@gmail.com>
2022-11-30 11:31:39 -0800

    form working, eventlistener working

--- create/create.js
+++ create/create.js
@@ -21,8 +21,16 @@ form.addEventListener('submit', async (e) => {
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
+    const select = document.querySelector('select');
     // go get the families from supabase
+    const families = await getFamilies();
     // for each family
+    for (let family of families) {
+        const option = document.createElement('option');
+        option.value = family.id;
+        option.textContent = family.name;
+        select.append(option);
+    }
     // create an option tag
     // set the option's value and text content
     // and append the option to the select
